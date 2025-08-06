3183c79cf17d9705ce500899a86e0e4a0450c9d0
Danielboah <danielboah409@gmail.com>
2025-08-06 03:19:24 -0700

    PROJECT WORK

---
+++ grade for students.py
@@ -0,0 +1,9 @@
+score=int(input("ENTER STUDENT SCORE"))
+if score>=90 and score <=100:
+    print("A")
+elif score>=80 and score<=89:
+    print("B") 
+elif score>=70 and score<=79:
+    print("C")  
+elif score<70:
+    print("F")     
